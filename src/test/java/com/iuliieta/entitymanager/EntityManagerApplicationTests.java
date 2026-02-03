431a4394b666622b97d1cb0ecc2509183b205714
Yulia Taratenko <iuliieta@Yulias-MacBook-Air.local>
2026-02-03 20:31:38 +0300

    Initial commit

---
+++ src/test/java/com/iuliieta/entitymanager/EntityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuliieta.entitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
